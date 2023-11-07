8d850ff94c2a46009cc32b4353a388db96abfcc7
Moumy Ndiaye <112096535+NMoumy@users.noreply.github.com>
2023-11-07 09:57:51 -0500

    Ajout de l'option pour quitter la description de cours

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,39 @@
 (function(){
 
 let elm__cours = document.querySelectorAll(".blocflex__cours");
+let btn__quitter = document.querySelectorAll(".btn__fermer");
+let superposition = document.querySelector(".superposition");
+let boiteActive = null;
 
 for (const elm of elm__cours) {
-  elm.addEventListener('mousedown', function(){
+  elm.addEventListener('mousedown', function(event){
+    event.stopPropagation();
     let id = this.id.split( '__')[0];
     console.log('console'+id);
     let boite = document.getElementById(id+'__boite');
-    boite.style.transform = 'scale(1)'
+    boite.style.transform = 'scale(1) translate(-50%, -50%)';
+    superposition.style.display = 'block';
+    boiteActive = boite;
   })
 }
+
+for (const btn of btn__quitter) {
+  btn.addEventListener('mousedown', function(event){
+    event.stopPropagation();
+    let id = this.parentNode.id.split('__')[0];
+    let boite = document.getElementById(id+'__boite');
+    boite.style.transform = 'scale(0) translate(-50%, -50%)';
+    superposition.style.display = 'none';
+    boiteActive = null;
+  }) 
+}
+
+superposition.addEventListener('mousedown', function(){
+  if (boiteActive) {
+    boiteActive.style.transform = 'scale(0) translate(-50%, -50%)';
+    superposition.style.display = 'none';
+    boiteActive = null;
+  }
+})
 
 })()
--- popup.php
+++ popup.php
@@ -9,9 +9,7 @@
 
 // Enqueue CSS et JavaScript
 function enqueue_popup_scripts() {
-  $version_css = filemtime(plugin_dir_path(__FILE__) . 'style.css');
   $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/popup.js');
-  wp_enqueue_style('popup-style', plugin_dir_url(__FILE__) . 'style.css', array(), $version_css);
   wp_enqueue_script('popup-script', plugin_dir_url(__FILE__) . 'js/popup.js', array(), $version_js, true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_popup_scripts');
