985e96a06edc503aebe92937efb1e4fe6ef6588f
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-11-19 00:45:22 -0500

    Ajout du code pour la section projet

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,10 @@
 (function(){
-
-let elm__cours = document.querySelectorAll(".blocflex__cours");
 let btn__quitter = document.querySelectorAll(".btn__fermer");
 let superposition = document.querySelector(".superposition");
+
+
+// Pour la section cours
+let elm__cours = document.querySelectorAll(".blocflex__cours");
 let boiteActive = null;
 
 for (const elm of elm__cours) {
@@ -21,18 +23,59 @@ for (const btn of btn__quitter) {
     event.stopPropagation();
     let id = this.parentNode.id.split('__')[0];
     let boite = document.getElementById(id+'__boite');
-    boite.style.transform = 'scale(0) translate(-50%, -50%)';
+    if (boite) {
+      boite.style.transform = 'scale(0) translate(-50%, -50%)';
+    }
     superposition.style.display = 'none';
     boiteActive = null;
   }) 
 }
 
-superposition.addEventListener('click', function(){
-  if (boiteActive) {
-    boiteActive.style.transform = 'scale(0) translate(-50%, -50%)';
-    superposition.style.display = 'none';
-    boiteActive = null;
+if(superposition){
+  superposition.addEventListener('click', function(){
+    if (boiteActive) {
+      boiteActive.style.transform = 'scale(0) translate(-50%, -50%)';
+      superposition.style.display = 'none';
+      boiteActive = null;
+    }
+  })
+}
+
+// Pour la section projets
+let elm__projets = document.querySelectorAll(".blocflex__projets");
+let infoActive = null;
+
+  for (const elm of elm__projets) {
+    elm.addEventListener('click', function(event){
+      event.stopPropagation();
+      let info = this.querySelector(".info__projet");
+      infoActive = info;
+      info.style.display = 'flex';
+      superposition.style.display = 'block';
+
+    })
   }
-})
+
+  for (const btn of btn__quitter) {
+    btn.addEventListener('click', function(event){
+      event.stopPropagation();
+      let info = document.querySelector(".info__projet");
+      if (info) {
+        info.style.display = 'none';
+        superposition.style.display = 'none';
+        infoActive = null;
+      }
+    }) 
+  }
+
+  if(superposition){
+    superposition.addEventListener('click', function(){
+      if (infoActive) {
+        infoActive.style.display = 'none';
+        superposition.style.display = 'none';
+        infoActive = null;
+      }
+    })
+  }
 
 })()
